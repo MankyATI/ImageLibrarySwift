3447dd19d287a920c787f8bdfa5519aa3cd5381d
Mayank <mayank.pahuja@kartrocket.com>
2018-01-14 13:33:53 +0530

    Initial Commit

---
+++ ImageLibraryDemoTests/ImageLibraryDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  ImageLibraryDemoTests.swift
+//  ImageLibraryDemoTests
+//
+//  Created by kraftly on 1/14/18.
+//  Copyright © 2018 Mayank. All rights reserved.
+//
+
+import XCTest
+@testable import ImageLibraryDemo
+
+class ImageLibraryDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
